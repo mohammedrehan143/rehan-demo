d7295dd13766149945a3a1db0e594c577e096606
mohammedrehan143 <mrofficialyt083@gmail.com>
2025-10-21 16:08:29 +0530

    new line added

--- assign1.py
+++ assign1.py
@@ -7,3 +7,27 @@ print(arr[1:4,1:4])
 # here we are slicing the matrix
 # 1to 4 mean 1to3 because it takes till n-1 and we r 
 # gettting this as (row1,row2,row3) etc etc
+
+# create an array with 10 numbers replace even number with oddnumber
+
+numm = np.array([1,2,3,4,5,6,7,8,9,10])
+for i in range(0,9):
+    if(numm[i]%2==0):
+        continue
+    else:
+        numm[i]+=1
+        
+print(numm)        
+
+
+# brpadcasting 
+
+arr1 = np.array([[1,2,3,4],
+                 [10,20,30,40],
+                 [23,34,45,56],
+                 [12,23,57,80]])
+
+mun = np.array([1,2,3,4])
+print("array after broadcasting=",arr1+mun)
+print((arr1.shape))
+print(arr1.reshape(2,8))
